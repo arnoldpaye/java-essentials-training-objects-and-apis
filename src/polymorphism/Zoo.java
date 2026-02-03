2919f341d2bacd05176e45c76da8152d7e2982f6
Arnold Paye <arnoldpayet@gmail.com>
2026-02-03 05:26:41 -0400

    instanceof operator

--- src/polymorphism/Zoo.java
+++ src/polymorphism/Zoo.java
@@ -6,11 +6,23 @@ public class Zoo {
     rocky.fetch();
     rocky.makeSound();
 
+    feed(rocky);
+
     Animal sasha = new Dog();
     sasha.makeSound();
 
     sasha = new Cat();
     sasha.makeSound();
     ((Cat)sasha).scratch();
+
+    feed(sasha);
+  }
+
+  public static void feed(Animal animal) {
+    if (animal instanceof  Dog) {
+      System.out.println("here's your dog food");
+    } else if (animal instanceof  Cat) {
+      System.out.println("here's your cat food");
+    }
   }
 }
